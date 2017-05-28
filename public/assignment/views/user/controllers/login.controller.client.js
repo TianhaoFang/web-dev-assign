230ec7b0bd290f881a8179b9bcfccbb1e0436805
FangTH <fang.tia@husky.neu.edu>
2017-05-28 16:51:30 -0400

    add empty controller LoginController in login.controller.client.js

---
+++ public/assignment/views/user/controllers/login.controller.client.js
@@ -0,0 +1,8 @@
+(function () {
+    angular
+        .module("WebAppMaker")
+        .controller("LoginController", function () {
+            var vm = this;
+            console.log("jump to LoginController");
+        })
+})();
