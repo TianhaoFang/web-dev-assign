e33acb253de326a8ad965d4d7aba19918c4e13c9
FangTH <fang.tia@husky.neu.edu>
2017-05-28 17:08:52 -0400

    add ng-href to replace href and add login action in the LoginController

--- public/assignment/views/user/controllers/login.controller.client.js
+++ public/assignment/views/user/controllers/login.controller.client.js
@@ -1,8 +1,13 @@
 (function () {
     angular
         .module("WebAppMaker")
-        .controller("LoginController", function () {
+        .controller("LoginController", function ($location) {
             var vm = this;
             console.log("jump to LoginController");
+            vm.username = "";
+            vm.password = "";
+            vm.login = function (user, password) {
+                alert("user:" + user + " password:" + password);
+            };
         })
 })();
