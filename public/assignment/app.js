cf1cb0c1b8781700f474bbbf9f32748c88e7fb4b
FangTH <fang.tia@husky.neu.edu>
2017-06-04 17:14:00 -0400

    modify the entry point and add bundle.js to the ignore file

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -1,4 +1,13 @@
+require("babel-polyfill");
+
 (function () {
     angular
         .module("WebAppMaker", ['ngRoute']);
+
+    async function lott() {
+        await new Promise((resolve, reject) => resolve(8));
+        return "abcddd";
+    }
+
+    lott().then(console.log);
 })();
