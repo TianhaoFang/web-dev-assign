67eb4af08b649ac55b46d789f88c9b52c9bb91f0
FangTH <fang.tia@husky.neu.edu>
2017-06-04 23:02:17 -0400

    create the call back suitable for the sortable element

--- public/assignment/views/widget/controllers/widget-list.controller.client.js
+++ public/assignment/views/widget/controllers/widget-list.controller.client.js
@@ -16,14 +16,43 @@
             return $sce.trustAsResourceUrl(originLink);
         };
 
+        vm.sortCallback = function (strList) {
+            const diff = findDiffElement(vm.sequence, strList);
+            console.log("diff", diff);
+            console.log(vm.sequence);
+            if(diff){
+                const [start, end] = diff;
+                vm.sequence.splice(end, 0, vm.sequence.splice(start, 1)[0]);
+                console.log(vm.sequence);
+            }
+            vm.sequence = strList;
+            console.log(vm.sequence);
+        };
+
         init();
 
         function init() {
             vm.currentUrl = "#!" + $location.url();
             vm.userId = userId;
+            vm.sequence = [];
             WidgetService.findWidgetsByPageId(pageId).then(list => {
                 vm.list = list;
+                vm.sequence = [];
+                for(let i = 0; i < list.length; i++){
+                    vm.sequence[i] = String(i);
+                }
             });
+        }
+
+        function findDiffElement(prevList, newList) {
+            let i = 0;
+            let j = 0;
+            for(; i < prevList.length && prevList[i] === newList[i]; i++){}
+            for(j = i; j < prevList.length && prevList[j] !== newList[j]; j++){}
+            j--;
+            if(i >= prevList.length) return null;
+            else if(prevList[i] === newList[j]) return [i, j];
+            else return [j, i];
         }
     }
 })();
