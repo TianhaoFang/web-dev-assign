533ce3b9150ff972fde85609de1ec03bd88a5de5
FangTH <fang.tia@husky.neu.edu>
2017-06-12 22:26:29 -0400

    refactor the page.service and widget.service

--- assignment/services/page.service.server.js
+++ assignment/services/page.service.server.js
@@ -1,3 +1,5 @@
+const Page = require("./../model/page.model.server");
+
 module.exports = function (app) {
     app.post("/api/website/:websiteId/page", createPage);
     app.get("/api/website/:websiteId/page", findAllPagesForWebsite);
@@ -5,59 +7,44 @@ module.exports = function (app) {
     app.put("/api/page/:pageId", updatePage);
     app.delete("/api/page/:pageId", deletePage);
 
-    let newId = 1001;
-    let pages = [
-        {"_id": "321", "name": "Post 1", "websiteId": "456", "description": "Lorem"},
-        {"_id": "432", "name": "Post 2", "websiteId": "456", "description": "Lorem"},
-        {"_id": "543", "name": "Post 3", "websiteId": "456", "description": "Lorem"}
-    ];
-
     // private functions
-    function genId() {
-        return String(newId++);
+    function sendNullable(res, pageId, result) {
+        if(result) res.json(result);
+        else genNotFind(res, pageId);
     }
 
     function genNotFind(res, pageId) {
         res.status(404).json({message: "not find page with id:" + pageId});
     }
 
-    function createPage(req, res){
+    async function createPage(req, res){
         const websiteId = req.params.websiteId;
         const page = req.body;
-        page._id = genId();
-        page.websiteId = websiteId;
-        pages.push(page);
-        res.json(page);
+        const newPage = await Page.createPage(websiteId, page);
+        if(!newPage) res.status(404).json({
+            message: "the websiteId:" + websiteId + " is not found"
+        });
+        res.json(newPage);
     }
 
-    function findAllPagesForWebsite(req, res){
+    async function findAllPagesForWebsite(req, res){
         const websiteId = req.params.websiteId;
-        res.json(pages.filter(elem => elem.websiteId === websiteId));
+        res.json(await Page.findAllPagesForWebsite(websiteId));
     }
 
-    function findPageById(req, res){
+    async function findPageById(req, res){
         const pageId = req.params.pageId;
-        const result = pages.find(elem => elem._id === pageId);
-        if(!result) genNotFind(res, pageId);
+        sendNullable(res, pageId, await Page.findPageById(pageId));
     }
 
-    function updatePage(req, res){
+    async function updatePage(req, res){
         const pageId = req.params.pageId;
         const page = req.body;
-        const oldPage = pages.find(elem => elem._id === pageId);
-        if(!oldPage) return genNotFind(res, pageId);
-        page._id = oldPage._id;
-        Object.assign(oldPage, page);
-        res.json(oldPage);
+        sendNullable(res, pageId, await Page.updatePage(pageId, page));
     }
 
-    function deletePage(req, res){
+    async function deletePage(req, res){
         const pageId = req.params.pageId;
-        for(let i = 0; i < pages.length; i++){
-            if(pages[i]._id === pageId){
-                return res.json(pages.splice(i, 1));
-            }
-        }
-        genNotFind(res, pageId);
+        sendNullable(res, pageId, await Page.deletePage(pageId))
     }
 };
--- assignment/services/widget.service.server.js
+++ assignment/services/widget.service.server.js
@@ -1,4 +1,5 @@
 let multer = require("multer");
+const Widget = require("./../model/widget.model.server");
 
 module.exports = function (app) {
     let upload = multer({
@@ -13,105 +14,62 @@ module.exports = function (app) {
     app.put("/api/page/:pageId/widget", reorderWidget);
     app.post("/api/upload", upload.single("myFile"), uploadImage);
 
-    // local widget data
-    var widgets = [
-        {"_id": "123", "widgetType": "HEADING", "pageId": "321", "size": 2, "text": "GIZMODO"},
-        {"_id": "234", "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
-        {
-            "_id": "345", "widgetType": "IMAGE", "pageId": "321", "width": "100%",
-            "url": "http://lorempixel.com/400/200/"
-        },
-        {"_id": "444", "widgetType": "HTML", "pageId": "123", "text": "sdadasdadad"},
-        {"_id": "456", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"},
-        {"_id": "567", "widgetType": "HEADING", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
-        {
-            "_id": "678", "widgetType": "YOUTUBE", "pageId": "321", "width": "100%",
-            "url": "https://youtu.be/AM2Ivdi9c4E"
-        },
-        {"_id": "789", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"}
-    ];
-
-    let newId = 1201;
-
-    function genId() {
-        return String(newId++);
-    }
-
     function outputNotFind(res, widgetId) {
         res.status(404).json({"message": "could not find widget by id" + widgetId});
     }
 
-    function createWidget(req, res) {
+    function sentNullable(res, widgetId, result) {
+        if(result) res.json(result);
+        else outputNotFind(res, widgetId);
+    }
+
+    async function createWidget(req, res) {
         const pageId = req.params.pageId;
         const widget = req.body;
-        widget._id = genId();
-        widget.pageId = pageId;
-        widgets.push(widget);
-        res.json(widget);
+        const result = await Widget.createWidget(pageId, widget);
+        if(!result) res.status(404).json({
+            message: "could not find pageId:" + pageId
+        });
+        res.json(result);
     }
 
-    function findAllWidgetsForPage(req, res) {
+    async function findAllWidgetsForPage(req, res) {
         const pageId = req.params.pageId;
-        res.json(widgets.filter(page => page.pageId === pageId));
+        res.json(await Widget.findAllWidgetsForPage(pageId));
     }
 
-    function findWidgetById(req, res) {
+    async function findWidgetById(req, res) {
         const widgetId = req.params.widgetId;
-        const result = widgets.find(page => page._id === widgetId);
-        if (!result) outputNotFind(res, widgetId);
-        else res.json(result);
+        sentNullable(res, widgetId, await Widget.findWidgetById(widgetId));
     }
 
-    function updateWidget(req, res) {
+    async function updateWidget(req, res) {
         const widgetId = req.params.widgetId;
         const widget = req.body;
-        widget._id = widgetId;
-        let oldValue = widgets.find(page => page._id === widgetId);
-        if (!oldValue) outputNotFind(res, widgetId);
-        else {
-            Object.assign(oldValue, widget);
-            res.json(oldValue);
-        }
+        sentNullable(res, widgetId, await Widget.updateWidget(widgetId, widget));
     }
 
-    function deleteWidget(req, res) {
+    async function deleteWidget(req, res) {
         const widgetId = req.params.widgetId;
-        for (let i = 0; i < widgets.length; i++) {
-            if (widgets[i]._id === widgetId) {
-                res.json(widgets.splice(i, 1));
-                return;
-            }
-        }
-        outputNotFind(res, widgetId);
+        sentNullable(res, widgetId, await Widget.deleteWidget(widgetId));
     }
 
-    function reorderWidget(req, res) {
+    async function reorderWidget(req, res) {
         const pageId = req.params.pageId;
         const initial = parseInt(req.query.initial);
         const final = parseInt(req.query.final);
         if ((!initial && initial !== 0) || (!final && final !== 0)) {
-            return res.status(400).json({message: "should have query: ?initial=num1&final=num2"});
+            return res.status(400).json({message: "should have query params initial and final with both int"});
         }
-
-        const imageList = Array.from(widgets.entries()).filter(pair => pair[1].pageId === pageId);
-
-        if (initial >= imageList.length || initial < 0) {
-            return res.status(400).json({
-                message: "initial=" + initial + " is out of boundary limit:" + imageList.length
-            });
-        }
-        if (final >= imageList.length || final < 0) {
-            return res.status(400).json({
-                message: "final=" + final + " is out of boundary limit:" + imageList.length
-            });
-        }
-
-        if (initial !== final) {
-            swapArray(widgets, imageList[initial][0], imageList[final][0]);
-            swapArray(imageList, initial, final);
-        }
-
-        res.json(imageList.map(pair => pair[1]));
+        const result = await Widget.reorderWidget(pageId, initial, final);
+        result
+            .onNotFound(x => res.status(404).json({message: "could not find pageId:" + pageId}))
+            .onOutBoundary(({name, limit, fact}) => {
+                res.status(400).json({
+                    message: "the " + name + ":" + fact + " is out of boundary [0, " + limit + ")"
+                })
+            })
+            .onSuccess(r => res.json(r))
     }
 
     // upload the file using the file reader to do that
@@ -121,11 +79,5 @@ module.exports = function (app) {
             file: myFile.filename,
             url: "/uploads/" + myFile.filename
         });
-    }
-
-
-    function swapArray(array, startId, endId) {
-        array.splice(endId, 0, array.splice(startId, 1)[0]);
-        return array;
     }
 };
