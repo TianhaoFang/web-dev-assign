543d04abff4dd5eb951b9b83e5b9b7fe891a47de
FangTH <fang.tia@husky.neu.edu>
2017-06-13 02:03:03 -0400

    implement the search graph and selection

--- public/assignment/views/widget/controllers/widget-flickr-search.controller.client.js
+++ public/assignment/views/widget/controllers/widget-flickr-search.controller.client.js
@@ -3,13 +3,34 @@
         .module("WebAppMaker")
         .controller("WidgetFlickrSearchController", WidgetFlickrSearchController);
 
-    function WidgetFlickrSearchController($location, $routeParams) {
+    function WidgetFlickrSearchController($location, $routeParams, FlickrService, WidgetService) {
         var vm = this;
         var userId = $routeParams['uid'];
         var websiteId = $routeParams['wid'];
         var pageId = $routeParams['pid'];
         var widgetId = $routeParams['wgid'];
         init();
+
+        vm.searchPhotos = function (text) {
+            if(!text) return;
+            FlickrService.searchPhotos(text).then(data => {
+                vm.photos = data.photos;
+            });
+        };
+
+        vm.generateUrl = function (photo) {
+            return "https://farm" + photo.farm + ".staticflickr.com/" + photo.server + "/"
+                + photo.id + "_" + photo.secret + "_s.jpg";
+        };
+
+        vm.selectPhoto = function (photo) {
+            const url = vm.generateUrl(photo);
+            return WidgetService.updateWidget(widgetId, {
+                url: url
+            }).then(() => {
+                $location.url($location.url().replace("/flickr", ""));
+            });
+        };
 
         function init() {
             vm.currentUrl = "#!" + $location.url();
