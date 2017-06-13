1250701a8b1f6db56b4103406b4dba307690169c
FangTH <fang.tia@husky.neu.edu>
2017-06-13 02:59:37 -0400

    add the new edit page the html editor, that include textAngular to do test format jobs

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -2,12 +2,5 @@ require("babel-polyfill");
 
 (function () {
     angular
-        .module("WebAppMaker", ['ngRoute', "wbdvDirectives"]);
-
-    async function lott() {
-        await new Promise((resolve, reject) => resolve(8));
-        return "abcddd";
-    }
-
-    lott().then(console.log);
+        .module("WebAppMaker", ['ngRoute', "wbdvDirectives", "textAngular"]);
 })();
